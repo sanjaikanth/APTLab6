4c36b74e850d004a4a86842368117cfc84af726b
sanjaikanth <sanjaikanth@gmail.com>
2019-10-09 14:28:47 -0500

    modified:   compute_highest_affinity.py

--- compute_highest_affinity.py
+++ compute_highest_affinity.py
@@ -28,16 +28,5 @@ def highest_affinity(site_list, user_list, time_list):
                 if pair not in affinities:
                     affinities[pair] = 0
                 affinities[pair] += 1
-    if 1==2:
-        for user, history in user_history.items():
-            history = list(history)
-            history.sort()
-            for i, site1 in enumerate(history):
-                for site2 in history[i + 1:]:
-                    pair = (site1, site2)
-                    if pair not in affinities:
-                        affinities[pair] = 0
-                    affinities[pair] += 1
-                    
     
     return max(affinities, key=affinities.get)
